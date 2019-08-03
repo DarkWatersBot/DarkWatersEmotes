f80e28e318ab48ac9af585921fd42e0b80d1b762
DarkWatersBot <53665198+DarkWatersBot@users.noreply.github.com>
2019-08-04 00:50:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,14 @@
-const { Client, Attachment } = require('discord.js');
-const client = new Client();
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
 client.on('ready', () => {
- console.log(`Logged in as ${client.user.tag}!`);
- });
-
-client.on('message', message => {
-	if (message.member.roles.find(r => r.name === "Owner") || message.member.roles.find(r => r.name === "Admin") || message.member.roles.find(r => r.name === "VIP") || message.member.roles.find(r => r.name === "Supporter")) {
-		if (message.content.includes('//sweat')){
-			const attachment = new Attachment('https://1.bp.blogspot.com/-LlVP0MWQ1o0/XS1kA72glxI/AAAAAAAAASc/q3P-jTyjVYkvAn-IWPRyHZ3Ce9srTyAlgCLcBGAs/s1600/ColdSweatVesta-Small.png');
-			message.channel.send(attachment);
-		}else if (message.content.includes('//lewd')){
-			const attachment = new Attachment('https://1.bp.blogspot.com/-fjfdovkmpkc/XS1kBZRPDKI/AAAAAAAAASo/xTjAFoZuZhcTSJwODgDBN6qVQ0cQPgI8gCLcBGAs/s1600/LewdSara.png');
-			message.channel.send(attachment);
-		}else if (message.content.includes('//shy')){
-			const attachment = new Attachment('https://1.bp.blogspot.com/-N47S_LD9WH4/XS1kBOqEBMI/AAAAAAAAASk/UW8p4ga-Vj4PfuvDqsyaIAE_11e6Dp6JACLcBGAs/s1600/FiaShy.png');
-			message.channel.send(attachment);
-		}else if (message.content.includes('//garxen')){
-			const attachment = new Attachment('https://1.bp.blogspot.com/-JVYwL9lN5F8/XS1kBBdQ2yI/AAAAAAAAASg/z74h4YcDPwIrAXQYFGoT2lCSbBRcW0fMgCLcBGAs/s1600/GarxenEmote.png');
-			message.channel.send(attachment);
-		}else if (message.content.includes('//braiowo')){
-			const attachment = new Attachment('https://1.bp.blogspot.com/-3Bj2t5ePkM0/XS8BxUnB1fI/AAAAAAAAATA/oeHSkuhRoZsXInAPjSAzgihc3Zv97yyhwCLcBGAs/s1600/image0-10.gif');
-			message.channel.send(attachment);
-    }}
+	console.log('I am ready!'):
 });
 
-client.login(process.env.BOT_TOKEN);
+client.on('message', message => {
+	if (message.content === 'ping') {
+		message.reply('pong');
+	}
+});
+
+client.loging(process.env.BOT_TOKEN);
