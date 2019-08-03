33ed384d0743172a05ceae6445918b2eae17fc8e
DarkWatersBot <53665198+DarkWatersBot@users.noreply.github.com>
2019-08-04 01:05:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,13 +2,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-	console.log('I am ready!'):
+    console.log('I am ready!');
 });
 
 client.on('message', message => {
-	if (message.content === 'ping') {
-		message.reply('pong');
-	}
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
-client.loging(process.env.BOT_TOKEN);
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot 
