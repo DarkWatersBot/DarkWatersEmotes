bf894d1b69fe3af8fb805ab67bb93a85ebd0ea52
DarkWatersBot <53665198+DarkWatersBot@users.noreply.github.com>
2019-08-09 04:48:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -76,6 +76,9 @@ client.on('message', message => {
 		}else if (message.content.includes('//whatislove')){
 			const attachment = new Attachment('https://1.bp.blogspot.com/-Zhp6UJ1GU8o/XUzRzNVQt0I/AAAAAAAAAUc/2tcoUY-1Ze038uqWOXO8i3y2VVN0ldP2gCLcBGAs/s1600/whatislove.gif');
 			message.channel.send(attachment);
+		}else if (message.content.includes('//renablush')){
+			const attachment = new Attachment('https://1.bp.blogspot.com/-fgyfgdNZbLQ/XUzes5NKGmI/AAAAAAAAAVU/FiOE6WIkfZYUex_QGOiE3stWk_DXwRxGwCLcBGAs/s1600/RenaBlush.png');
+			message.channel.send(attachment);
 		}else if (message.content.includes('//progress') && message.channel.id === '595828458135879680'){
 			const attachment = new Attachment('https://indarkwaters.lima-city.de/progress/progress.png');
 			message.channel.send(attachment);
